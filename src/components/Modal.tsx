a2dcc3e8bfd1fe93095f8c3ce863d8484ee9e024
pabiak <pabiak17@gmail.com>
2025-06-20 17:52:59 +0200

    add recipes page, and modal component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,116 @@
+import { createPortal } from "react-dom";
+
+import CloseIcon from "@/assets/close.svg?react";
+
+interface IModalProps {
+  name: string;
+  onClose: () => void;
+  onRecipeDelete: (recipeId: string) => void;
+  isOpen: boolean;
+  recipe: IRecipe | null;
+}
+
+interface IIngredient {
+  name: string;
+  amount: string;
+}
+
+interface IParameter {
+  name: string;
+  value: string;
+}
+
+export interface IRecipe {
+  id: string;
+  name: string;
+  createdAt: string;
+  ingredients: Array<IIngredient>;
+  parameters: Array<IParameter>;
+}
+
+const Modal = ({
+  name,
+  onClose,
+  isOpen,
+  recipe,
+  onRecipeDelete,
+}: IModalProps) => {
+  if (!isOpen) return null;
+
+  const handleDelete = () => {
+    if (!recipe.id) return;
+    onClose();
+    onRecipeDelete(recipe.id);
+  };
+
+  return createPortal(
+    <button
+      className="fixed inset-0 z-40 transition-opacity duration-[.4s] w-full flex items-center justify-center p-6 backdrop-blur-xs bg-gray-400/70"
+      onClick={onClose || undefined}
+      data-name={name}
+      role="dialog"
+    >
+      <div
+        role="button"
+        onKeyDown={(e) => e.stopPropagation()}
+        onClick={(e) => e.stopPropagation()}
+        className="flex flex-col w-full bg-white rounded-xl max-h-[98svh] overflow-y-auto min-w-[20rem] lg:min-w-[65rem] raise-from-center"
+      >
+        <header className="flex items-center justify-between p-4 border-b border-gray-200">
+          <h2 className="text-lg font-semibold text-[var(--text)]">
+            {recipe.name}
+          </h2>
+          <button
+            onClick={onClose}
+            className="text-[var(--text)] focus:outline-none"
+            aria-label="Close modal"
+          >
+            <CloseIcon />
+          </button>
+        </header>
+        <div className="p-4">
+          <h3 className="text-md font-semibold text-[var(--text)]">
+            Składniki
+          </h3>
+          <ul className="space-y-2">
+            {recipe.ingredients.map((ingredient: IIngredient) => (
+              <li
+                key={ingredient.name}
+                className="flex justify-between text-[var(--placeholder)]"
+              >
+                <span className="text-[var(--text)]">{ingredient.name}</span>
+                <span className="text-[var(--placeholder)]">
+                  {ingredient.amount}
+                </span>
+              </li>
+            ))}
+          </ul>
+          <h3 className="mt-4 text-md font-semibold text-[var(--text)]">
+            Parametry
+          </h3>
+          <ul className="space-y-2 mt-2">
+            {recipe.parameters.map((parameter: IParameter) => (
+              <li key={parameter.name} className="flex justify-between">
+                <span className="text-[var(--text)]">{parameter.name}</span>
+                <span className="text-[var(--placeholder)]">
+                  {parameter.value}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <footer className="p-4 border-t border-gray-200">
+          <button
+            onClick={handleDelete}
+            className="w-full h-12 rounded-3xl border text-[var(--primary)] bg-[var(--background)] border-[var(--primary)]"
+          >
+            Usuń przepis
+          </button>
+        </footer>
+      </div>
+    </button>,
+    document.body,
+  );
+};
+
+export default Modal;
--- src/pages/RecipesPage.tsx
+++ src/pages/RecipesPage.tsx
@@ -1,7 +1,146 @@
-import React from "react";
+import getFormattedDate from "@/helpers/formatDate";
+import PizzaImg from "@/assets/pizza.png";
+import Modal, { IRecipe } from "@/components/Modal";
+import { useState } from "react";
 
 const RecipesPage = () => {
-  return <div>RecipesPage</div>;
+  const exampleRecipes = [
+    {
+      id: crypto.randomUUID(),
+      name: "przepis1",
+      createdAt: "2023-10-01",
+      ingredients: [
+        { name: "Mąka", amount: "500g" },
+        { name: "Woda", amount: "300ml" },
+        { name: "Sól", amount: "10g" },
+        { name: "Drożdże świeże", amount: "5g" },
+        { name: "Oliwa", amount: "20g" },
+      ],
+      parameters: [
+        { name: "Liczba porcji", value: "2" },
+        { name: "Waga Porcji", value: "250g" },
+        { name: "Hydracja", value: "60%" },
+        { name: "Czas całkowity", value: "24h" },
+        { name: "Czas w temperaturze pokojowej", value: "2h" },
+        { name: "Czas w lodówce", value: "12h" },
+      ],
+    },
+    {
+      id: crypto.randomUUID(),
+      name: "przepis2",
+      createdAt: "2023-10-02",
+      ingredients: [
+        { name: "Mąka", amount: "600g" },
+        { name: "Woda", amount: "350ml" },
+        { name: "Sól", amount: "12g" },
+        { name: "Drożdże świeże", amount: "6g" },
+        { name: "Oliwa", amount: "25g" },
+      ],
+      parameters: [
+        { name: "Liczba porcji", value: "2" },
+        { name: "Waga Porcji", value: "250g" },
+        { name: "Hydracja", value: "60%" },
+        { name: "Czas całkowity", value: "24h" },
+        { name: "Czas w temperaturze pokojowej", value: "2h" },
+        { name: "Czas w lodówce", value: "12h" },
+      ],
+    },
+  ];
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedRecipe, setSelectedRecipe] = useState<IRecipe | null>(null);
+
+  const handleCloseModal = () => {
+    setIsOpen(false);
+  };
+
+  const handleOpenModal = (recipe: IRecipe) => {
+    if (!recipe) return;
+    setSelectedRecipe(recipe);
+    setIsOpen(true);
+  };
+
+  const handleDeleteRecipe = (recipeId: string) => {
+    // Implement the logic to delete the recipe by its ID
+    console.log(`Deleting recipe with ID: ${recipeId}`);
+  };
+
+  return (
+    <div className="grid grid-cols-1 gap-3 mb-[4.25rem]">
+      <h1 className="font-bold text-lg text-[var(--text)] my-5 text-center tracking-normal leading-none">
+        Przepisy
+      </h1>
+      <Modal
+        name="recipe-modal"
+        isOpen={isOpen}
+        onClose={handleCloseModal}
+        recipe={selectedRecipe}
+        onRecipeDelete={handleDeleteRecipe}
+      />
+      {exampleRecipes.map((recipe) => (
+        <button
+          onClick={() => handleOpenModal(recipe)}
+          type="button"
+          aria-label="Open recipe modal"
+          key={recipe.id}
+          className="w-full text-left p-4 bg-white rounded-lg shadow-md flex gap-2 relative"
+        >
+          <figure className="flex-shrink-0">
+            <img
+              src={PizzaImg}
+              alt="Placeholder - pizza image"
+              className="w-[5rem]"
+            />
+          </figure>
+          <p className="absolute top-[12px] right-[12px] text-sm text-gray-600 mb-2">
+            {getFormattedDate(new Date(recipe.createdAt))}
+          </p>
+          <div className="flex flex-col justify-center">
+            <h2 className="font-bold text-lg text-[var(--text)]">
+              {recipe.name}
+            </h2>
+            <ul className="flex gap-4">
+              <li className="flex flex-col">
+                <span className="text-[var(--placeholder)] text-xs">
+                  Liczba porcji:
+                </span>
+                <span className="text-[var(--text)] text-center">
+                  {
+                    recipe.parameters.find(
+                      (param) => param.name === "Liczba porcji",
+                    )?.value
+                  }
+                </span>
+              </li>
+              <li className="flex flex-col">
+                <span className="text-[var(--placeholder)] text-xs">
+                  Waga porcji:
+                </span>
+                <span className="text-[var(--text)] text-center">
+                  {
+                    recipe.parameters.find(
+                      (param) => param.name === "Waga Porcji",
+                    )?.value
+                  }
+                </span>
+              </li>
+              <li className="flex flex-col">
+                <span className="text-[var(--placeholder)] text-xs">
+                  Hydracja:
+                </span>
+                <span className="text-[var(--text)] text-center">
+                  {
+                    recipe.parameters.find((param) => param.name === "Hydracja")
+                      ?.value
+                  }
+                </span>
+              </li>
+            </ul>
+          </div>
+        </button>
+      ))}
+    </div>
+  );
 };
 
 export default RecipesPage;
